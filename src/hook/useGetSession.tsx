2a99b9eb4fa04586ab2da01f6691431a3ceb382d
Quoc Dinh <dinhanhquoc2525@gmail.com>
2025-04-03 12:14:39 +0700

    feat: make login feature and protected route

--- src/hook/useGetSession.tsx
+++ src/hook/useGetSession.tsx
@@ -6,9 +6,7 @@ const useGetSession = () => {
   const dispatch = useAppDispatch();
   const data = useAppSelector((state) => state.user);
   useEffect(() => {
-    dispatch(getUserSession())
-      .unwrap()
-      .catch((error) => console.log(error));
+    dispatch(getUserSession()).unwrap();
   }, []);
   return { user: data.user, loading: data.loading };
 };

5074b59302f798c732e8b0b878748b16fa2df78c
Quoc Dinh <dinhanhquoc2525@gmail.com>
2025-05-02 13:38:46 +0700

    fix: change ui

--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -1,10 +1,11 @@
-import { ListNotification } from "@/features/leave-request";
-import { UpdateLeaveRequest } from "@/features/leave-request";
+import { ListNotification, UpdateLeaveRequest } from "@/features/leave-request";
+import { UserDetail } from "@/features/user";
 const Notification = () => {
   return (
     <div>
       <ListNotification />
       <UpdateLeaveRequest />
+      <UserDetail />
     </div>
   );
 };
